d0749e7c5efb6368b34d44938100eef91011d271
shlee4290 <shlee4290@naver.com>
2020-03-29 14:15:05 +0900

    project1 complete

--- project1/delete.c
+++ project1/delete.c
@@ -4,65 +4,83 @@
 #include <fcntl.h>
 #include <string.h>
 
-#define READ_WRITE_BYTES 100
+#define READ_WRITE_BYTES 100 // 100 byte씩 입출력
 
 long parseLong(const char *numString);
 
 int main(int argc, char *argv[])
 {
-	char *buffer;
-	char *entry;
-	int fd;
-	int length;
-	int deleteBytes;
-	int fileSize;
-	int i;
-	int offset;
+	char *buffer; // 파일 입출력에 사용할 버퍼
+	char *entry; // 읽어들인 데이터를 버퍼에 쓸 위치를 저장해 놓을 변수
+	int fd; // 파일 디스크립터 변수
+	int length; // 입출력된 길이를 저장할 변수
+	int deleteBytes; // delete할 데이터의 길이
+	int fileSize; // 파일의 크기
+	int deletedFileLength; // delete 후의 파일의 크기
+	int i; // 반복문에 사용할 인덱스
+	int offset; // 파일의 오프셋
 
-
-	if(argc != 4){
+	if(argc != 4){ // 프로그램에 전달된 인자의 수가 잘못됐다면 프로그램 종료
 		fprintf(stderr
 			, "usage : %s <file> <offset> <delete bytes>\n"
 			, argv[0]);
 		exit(1);
 	}
 
-	if((fd = open(argv[1], O_RDONLY)) < 0){
+	if((fd = open(argv[1], O_RDONLY)) < 0){ // 첫번째 인자로 전달된 file 읽기 전용 모드로 open
 		fprintf(stderr, "open error for %s\n", argv[1]);
 		exit(1);
 	}
 
-	if((fileSize = lseek(fd, 0, SEEK_END)) < 0){
+	if((fileSize = lseek(fd, 0, SEEK_END)) < 0){ // open한 파일의 크기 저장
 		fprintf(stderr, "lseek error\n");
 		exit(1);
 	}
 
-	offset = parseLong(argv[2]);
-	deleteBytes = parseLong(argv[3]);
+	offset = parseLong(argv[2]); // 인자로 전달된 offset long형으로 변환하여 저장
+	deleteBytes = parseLong(argv[3]); // 인자로 전달된 delete할 길이 저장
 
-	buffer = (char *)malloc(fileSize);
-	if(buffer == NULL){
+	if(offset > fileSize || !deleteBytes) // delete할 것이 없다면 바로 종료
+		exit(0);
+
+	// delete 후의 데이터 길이를 계산하여 저장
+	if((fileSize - (offset + deleteBytes)) < 0)
+		deletedFileLength = offset;
+	else
+		deletedFileLength = offset + fileSize - (offset + deleteBytes);	
+
+	buffer = (char *)malloc(fileSize); // 파일의 크기만큼 버퍼 메모리 할당
+	if(buffer == NULL){ // 메모리 할당 실패했다면 프로그램 종료
 		fprintf(stderr, "malloc error\n");
 		exit(1);
 	}
 
 	lseek(fd, 0, SEEK_SET);
 	entry = buffer;
-	while((length = read(fd, entry, READ_WRITE_BYTES)) > 0)
+	while((length = read(fd, entry, READ_WRITE_BYTES)) > 0) // 100바이트씩 읽어들인다
 		entry += length;
-	close(fd);
+	close(fd); // 읽기 종료 후 파일 close
 
-	if(fileSize > offset){
-		if((fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC)) < 0){
-			fprintf(stderr, "open error for %s\n", argv[1]);
-			exit(1);
-		}
-		write(fd, buffer, offset);
-		if((fileSize - (offset + deleteBytes)) > 0)
-			write(fd, buffer + offset + deleteBytes
-				, fileSize - (offset + deleteBytes));
+	for(i = offset; i < deletedFileLength; ++i){ // delete
+		buffer[i] = buffer[i + deleteBytes];
 	}
-	close(fd);
+
+	if((fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC)) < 0){ // delete 완료한 데이터를 파일에 저장하기 위해 파일을 초기화 하여 다시 open
+		fprintf(stderr, "open error for %s\n", argv[1]);
+		exit(1);
+	}
+	entry = buffer;
+	while(1) { // delete 완료된 데이터를 100바이트씩 파일에 출력
+		if(entry + READ_WRITE_BYTES >= buffer + deletedFileLength){
+			write(fd, entry, (buffer + deletedFileLength) - entry);
+			break;
+		}
+		length = write(fd, entry, READ_WRITE_BYTES); 
+		entry += length;
+	}
+	
+	close(fd); // 할당 했던 메모리 free
+	free(buffer);
 
 	exit(0);	
 }
--- project1/read.c
+++ project1/read.c
@@ -4,35 +4,42 @@
 #include <fcntl.h>
 #include <string.h>
 
-long parseLong(const char *numString);
+#define BUFFER_LENGTH 101 // 100 byte씩 읽고 쓴다. 나머지 1비트는 널문자를 위한것
+
+long parseLong(const char *numString); // 문자열로 된 숫자를 long형으로 변환하는 함수
 
 int main(int argc, char *argv[])
 {
-	char c;
-	int fd;
-	off_t numOfBytes;
-	off_t offset;
+	char buffer[BUFFER_LENGTH]; // 파일 입출력에 사용할 버퍼
+	int fd; // 파일 디스크립터 변수
+	int length; // 입출력된 길이를 저장할 변수
+	int numOfReadBytes; // 사용자에게 보여줄 데이터의 길이 저장하는 변수 
+	off_t offset; // 사용자에게 보여줄 데이터의 시작 위치 저장하는 변수
 
-	if(argc != 4) {
+	if(argc != 4) { // 전달받은 인자의 개수가 잘못되었다면 프로그램 종료
 		fprintf(stderr,"usage : %s <file> <offset> <bytes>\n", argv[0]);
 		exit(1);
 	}
 
-	if((fd = open(argv[1], O_RDONLY)) < 0) {
+	if((fd = open(argv[1], O_RDONLY)) < 0) { // 첫번째 인자로 전달받은 파일 오픈
 		fprintf(stderr, "open error for %s\n", argv[1]);
 		exit(1);
 	}
 
-	offset = parseLong(argv[2]);
-	if(lseek(fd, offset, SEEK_SET) < 0){
+	offset = parseLong(argv[2]); // 인자로 전달받은 오프셋 long형으로 변환 해 저장
+	if(lseek(fd, offset, SEEK_SET) < 0){ // offset 위치로 이동
 		fprintf(stderr, "lseek error\n");
 		exit(1);
 	}
 
-	numOfBytes = parseLong(argv[3]);
-	while(read(fd, &c, 1) > 0 && numOfBytes > 0) {
-		printf("%c", c);
-		--numOfBytes;
+	numOfReadBytes = parseLong(argv[3]); // 읽고 출력할 데이터의 수 저장
+	while((length = read(fd, buffer, BUFFER_LENGTH - 1)) > 0 && numOfReadBytes > 0) { // 파일에서 100바이트씩 읽어서 사용자에게 보여줌
+		buffer[length] = 0;
+		if(numOfReadBytes < length){
+			buffer[numOfReadBytes] = 0;
+		}
+		printf("%s", buffer);
+		numOfReadBytes -= length;
 	}
 	printf("\n");
 
@@ -40,13 +47,14 @@ int main(int argc, char *argv[])
 	exit(0);
 }
 
+// 문자열로 되어있는 숫자를 long형으로 변환하여 리턴하는 함수
 long parseLong(const char *numString){
 	int length = strlen(numString);
 	int i;
 	long number = 0;
 
 	for(i = 0; i < length; ++i){
-		if(numString[i] < '0' || numString[i] > '9') {
+		if(numString[i] < '0' || numString[i] > '9') { // 숫자가 아니라면 프로그램 종료
 			fprintf(stderr, "wrong argument\n");
 			exit(1);
 		}
